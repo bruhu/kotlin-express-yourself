3b49d9b04c5bfebaf775e79529491271efed925c
bruhu <annabru.abh@gmail.com>
2020-02-04 16:09:02 +0100

    Oh hi

---
+++ app/src/main/java/com/bruhu/expressyourself/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.bruhu.expressyourself
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val name = "Nolan Bushnell"
+        val yearOfBirth = "1943"
+        var currentYear = "2020"
+        var age: Int
+    }
+}
